aa44dd03575dff437b9484c5e21018e6264183d3
Jean9310 <jean_g93@hotmail.com>
2022-11-07 22:30:38 -0500

    enlace de todas las ramas

--- front-administracion/src/app/app-routing.module.ts
+++ front-administracion/src/app/app-routing.module.ts
@@ -15,24 +15,48 @@ const routes: Routes = [
     redirectTo: "/inicio"
   },
   {
-    path:"**",
-    component: ErrorComponent
+    path : 'administracion',
+    loadChildren : ()=> import ("./modulos/administracion/administracion.module").then(a => a.AdministracionModule)
   },
   {
-    path : 'administracion',
-    loadChildren : ()=> import ("./modulos/administracion/administracion.module").then(x => x.AdministracionModule)
+    path:'conjunto',
+    loadChildren :() => import('./modulos/conjunto/conjunto.module').then(c => c.ConjuntoModule)
   },
+  {
+    path:'contador',
+    loadChildren: () => import('./modulos/contador/contador.module').then(con => con.ContadorModule)
+  },
+  {
+    path:'factura',
+    loadChildren: ()=>import('./modulos/factura/factura.module').then(f =>f.FacturaModule)
+    },
+    {
+      path:'inmueble',
+      loadChildren: ()=>import('./modulos/inmueble/inmueble.module').then(i => i.InmuebleModule)
+    },
   {
     path:'login',
-    loadChildren :() => import('./modulos/login/login.module').then(x => x.LoginModule)
+    loadChildren :() => import('./modulos/login/login.module').then(l => l.LoginModule)
+  },
+  {
+    path: 'propietario',
+    loadChildren : () => import ('./modulos/propietario/propietario.module').then(p => p.PropietarioModule)
+  },
+  {
+    path:'seccion',
+    loadChildren: () => import('./modulos/seccion/seccion.module').then(s => s.SeccionModule)
+  },
+  {
+    path:'transaccion',
+    loadChildren:()=>import('./modulos/transaccion/transaccion.module').then(t => t.TransaccionModule)
   },
   {
     path: 'zona-social',
     loadChildren : () => import('./modulos/zona-social/zona-social.module') .then( z => z.ZonaSocialModule)
   },
   {
-    path: 'propietario',
-    loadChildren : () => import ('./modulos/propietario/propietario.module').then(x => x.PropietarioModule)
+    path:"**",
+    component: ErrorComponent
   }
 ];
 
